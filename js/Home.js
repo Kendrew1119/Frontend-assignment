cec86ffccca187610637d3fd9545fe5bf40558d3
Kendrew1119 <kendrewlim@yahoo.com>
2025-08-21 22:11:16 +0800

    home

--- js/Home.js
+++ js/Home.js
@@ -1,19 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
     // Search Bar functionality
     const searchButton = document.querySelector('.SearchButton');
     const searchPanel = document.getElementById('SearchPanel');
-    const closeSearchButton = document.getElementById('CloseSearchButton');
 
-    if (searchButton && searchPanel && closeSearchButton) {
+    if (searchButton && searchPanel) {
         searchButton.addEventListener('click', () => {
             searchPanel.classList.add('active');
             document.body.style.overflow = 'hidden'; // Disable scroll
-        });
-
-        closeSearchButton.addEventListener('click', () => {
-            searchPanel.classList.remove('active');
-            document.body.style.overflow = ''; // Re-enable scroll
         });
     }
 
@@ -99,3 +92,13 @@ function viewImage(img) {
   // e.g., open a modal or preview image
   alert("Image clicked: " + img.src);
 }
+
+window.addEventListener('message', (event) => {
+    if (event.data === 'closeSearchPanel') {
+        const searchPanel = document.getElementById('SearchPanel');
+        if (searchPanel) {
+            searchPanel.classList.remove('active');
+            document.body.style.overflow = ''; // Re-enable scroll
+        }
+    }
+});
